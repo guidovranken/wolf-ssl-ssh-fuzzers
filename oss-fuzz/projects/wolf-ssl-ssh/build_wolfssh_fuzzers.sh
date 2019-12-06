64f49fab52e7ac0f68e4b9d780f13c3dd3e72ec2
Guido Vranken <guidovranken@gmail.com>
2019-12-06 15:00:38 +0100

    Build intensity guided fuzzers

--- oss-fuzz/projects/wolf-ssl-ssh/build_wolfssh_fuzzers.sh
+++ oss-fuzz/projects/wolf-ssl-ssh/build_wolfssh_fuzzers.sh
@@ -5,30 +5,62 @@ mkdir -p $SRC/fuzzers/wolfssh/
 cp -R $SRC/wolfssh/ $SRC/fuzzers/wolfssh/
 cp -R $SRC/wolfssl/ $SRC/fuzzers/wolfssh/
 cp -R $SRC/fuzzers/wolfssh-fuzzers $SRC/fuzzers/wolfssh/wolfssh/
+cp -R $SRC/fuzzers/wolfssh/wolfssl/ $SRC/fuzzers/wolfssh/wolfssl_trace_pc_guard/
+cp -R $SRC/fuzzers/wolfssh/wolfssh/ $SRC/fuzzers/wolfssh/wolfssh_trace_pc_guard/
 
 export CFLAGS="$CFLAGS -DWOLFSSL_STATIC_PSK"
+export ORIGINAL_CFLAGS="$CFLAGS"
 
-# Build wolfSSL (required for wolfSSH)
-    cd $SRC/fuzzers/wolfssh/wolfssl/
-    autoreconf -ivf
-    ./configure $WOLFSSL_BASE_CONFIGURE_PARAMS --enable-ssh --enable-keygen
-    make -j$(nproc)
+# Build everything with -fsanitize=fuzzer-no-link (normal code coverage guided fuzzing)
+    # Build wolfSSL (required for wolfSSH)
+        cd $SRC/fuzzers/wolfssh/wolfssl/
+        autoreconf -ivf
+        ./configure $WOLFSSL_BASE_CONFIGURE_PARAMS --enable-ssh --enable-keygen
+        make -j$(nproc)
 
-# Required to configure + build wolfSSH without having to install wolfSSL
-    export LDFLAGS="-L$(realpath src/.libs/)"
-    export CFLAGS="$CFLAGS -I $(realpath .)"
+    # Required to configure + build wolfSSH without having to install wolfSSL
+        export LDFLAGS="-L$(realpath src/.libs/)"
+        export CFLAGS="$ORIGINAL_CFLAGS -I $(realpath .)"
 
-# Build wolfSSH
-    cd $SRC/fuzzers/wolfssh/wolfssh/
-    autoreconf -ivf
-    ./configure --enable-static --enable-all --disable-examples
-    make -j$(nproc)
+    # Build wolfSSH
+        cd $SRC/fuzzers/wolfssh/wolfssh/
+        autoreconf -ivf
+        ./configure --enable-static --enable-all --disable-examples
+        make -j$(nproc)
 
-# Build wolfSSH fuzzers
-    cd $SRC/fuzzers/wolfssh/wolfssh/wolfssh-fuzzers
+    # Build wolfSSH fuzzers
+        cd $SRC/fuzzers/wolfssh/wolfssh/wolfssh-fuzzers
 
-    make -B fuzzer-client
-    make -B fuzzer-server
+        make -B fuzzer-client
+        make -B fuzzer-server
 
-    cp fuzzer-client $OUT/fuzzer-wolfssh-client
-    cp fuzzer-server $OUT/fuzzer-wolfssh-server
+        cp fuzzer-client $OUT/fuzzer-wolfssh-client
+        cp fuzzer-server $OUT/fuzzer-wolfssh-server
+
+# Build everything with -fsanitize-coverage=trace-pc-guard (for intensity and allocation guided fuzzing)
+    export ORIGINAL_CFLAGS=${ORIGINAL_CFLAGS/"-fsanitize=fuzzer-no-link"/"-fsanitize-coverage=trace-pc-guard"}
+
+    # Build wolfSSL (required for wolfSSH)
+        cd $SRC/fuzzers/wolfssh/wolfssl_trace_pc_guard/
+        autoreconf -ivf
+        ./configure $WOLFSSL_BASE_CONFIGURE_PARAMS --enable-ssh --enable-keygen
+        make -j$(nproc)
+
+    # Required to configure + build wolfSSH without having to install wolfSSL
+        export LDFLAGS="-L$(realpath src/.libs/)"
+        export CFLAGS="$ORIGINAL_CFLAGS -I $(realpath .)"
+
+    # Build wolfSSH
+        cd $SRC/fuzzers/wolfssh/wolfssh_trace_pc_guard/
+        autoreconf -ivf
+        ./configure --enable-static --enable-all --disable-examples
+        make -j$(nproc)
+
+    # Build wolfSSH fuzzers (intensity guided)
+        cd $SRC/fuzzers/wolfssh/wolfssh_trace_pc_guard/wolfssh-fuzzers
+
+        make -B fuzzer-client-intensity
+        make -B fuzzer-server-intensity
+
+        cp fuzzer-client-intensity $OUT/fuzzer-wolfssh-client-intensity
+        cp fuzzer-server-intensity $OUT/fuzzer-wolfssh-server-intensity
--- oss-fuzz/projects/wolf-ssl-ssh/build_wolfssl_fuzzers.sh
+++ oss-fuzz/projects/wolf-ssl-ssh/build_wolfssl_fuzzers.sh
@@ -4,22 +4,44 @@ rm -rf $SRC/fuzzers/wolfssl/
 mkdir -p $SRC/fuzzers/wolfssl/
 cp -R $SRC/wolfssl/ $SRC/fuzzers/wolfssl/
 cp -R $SRC/fuzzers/wolfssl-fuzzers $SRC/fuzzers/wolfssl/wolfssl/
+cp -R $SRC/fuzzers/wolfssl/wolfssl/ $SRC/fuzzers/wolfssl/wolfssl_trace_pc_guard/
 
 export CFLAGS="$CFLAGS -DWOLFSSL_STATIC_PSK"
+export WOLFSSL_CONFIGURE_PARAMS="$WOLFSSL_BASE_CONFIGURE_PARAMS --enable-tls13 --enable-ocsp --enable-dtls --enable-sni --enable-blake2 --enable-blake2s --enable-curve25519 --enable-session-ticket --enable-nullcipher --enable-crl --enable-ed25519 --enable-psk --enable-earlydata --enable-postauth --enable-hrrcookie --enable-opensslextra --enable-certext --enable-tlsx --enable-oldtls --enable-tlsv10 --enable-indef --enable-psk"
 
-# Build wolfSSL
-    cd $SRC/fuzzers/wolfssl/wolfssl/
-    autoreconf -ivf
-    ./configure $WOLFSSL_BASE_CONFIGURE_PARAMS --enable-tls13 --enable-ocsp --enable-dtls --enable-sni --enable-blake2 --enable-blake2s --enable-curve25519 --enable-session-ticket --enable-nullcipher --enable-crl --enable-ed25519 --enable-psk --enable-earlydata --enable-postauth --enable-hrrcookie --enable-opensslextra --enable-certext --enable-tlsx --enable-oldtls --enable-tlsv10 --enable-indef --enable-psk
-    make -j$(nproc)
+# Build everything with -fsanitize=fuzzer-no-link (normal code coverage guided fuzzing)
+    # Build wolfSSL
+        cd $SRC/fuzzers/wolfssl/wolfssl/
+        autoreconf -ivf
+        ./configure $WOLFSSL_CONFIGURE_PARAMS
+        make -j$(nproc)
 
-# Build wolfSSL fuzzers
-    cd $SRC/fuzzers/wolfssl/wolfssl/wolfssl-fuzzers
+    # Build wolfSSL fuzzers (code coverage guided)
+        cd $SRC/fuzzers/wolfssl/wolfssl/wolfssl-fuzzers
 
-    make -B fuzzer-misc
-    make -B fuzzer-crl
-    make -B fuzzer-ocsp
+        make -B fuzzer-misc
+        make -B fuzzer-crl
+        make -B fuzzer-ocsp
 
-    cp fuzzer-misc $OUT/fuzzer-wolfssl-misc
-    cp fuzzer-crl $OUT/fuzzer-wolfssl-crl
-    cp fuzzer-ocsp $OUT/fuzzer-wolfssl-ocsp
+        cp fuzzer-misc $OUT/fuzzer-wolfssl-misc
+        cp fuzzer-crl $OUT/fuzzer-wolfssl-crl
+        cp fuzzer-ocsp $OUT/fuzzer-wolfssl-ocsp
+
+# Build everything with -fsanitize-coverage=trace-pc-guard (for intensity and allocation guided fuzzing)
+    export CFLAGS=${CFLAGS/"-fsanitize=fuzzer-no-link"/"-fsanitize-coverage=trace-pc-guard"}
+    # Build wolfSSL
+        cd $SRC/fuzzers/wolfssl/wolfssl_trace_pc_guard/
+        autoreconf -ivf
+        ./configure $WOLFSSL_CONFIGURE_PARAMS
+        make -j$(nproc)
+
+    # Build wolfSSL fuzzers (intensity guided)
+        cd $SRC/fuzzers/wolfssl/wolfssl_trace_pc_guard/wolfssl-fuzzers
+
+        make -B fuzzer-misc-intensity
+        make -B fuzzer-crl-intensity
+        make -B fuzzer-ocsp-intensity
+
+        cp fuzzer-misc-intensity $OUT/fuzzer-wolfssl-misc-intensity
+        cp fuzzer-crl-intensity $OUT/fuzzer-wolfssl-crl-intensity
+        cp fuzzer-ocsp-intensity $OUT/fuzzer-wolfssl-ocsp-intensity
