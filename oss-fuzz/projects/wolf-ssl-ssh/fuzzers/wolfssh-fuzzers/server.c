6d04dddeacdd97b7a6bb6c0c1e0de283bbbbd019
Guido Vranken <guidovranken@gmail.com>
2021-01-08 23:47:59 +0100

    wolfSSH server: Implement SCP IO hooks

--- oss-fuzz/projects/wolf-ssl-ssh/fuzzers/wolfssh-fuzzers/server.c
+++ oss-fuzz/projects/wolf-ssl-ssh/fuzzers/wolfssh-fuzzers/server.c
@@ -1,3 +1,4 @@
+#define WOLFSSH_SCP
 #include <wolfssh/ssh.h>
 #include <wolfssh/wolfscp.h>
 #include <fuzzers/shared.h>
@@ -19,6 +20,23 @@ int fuzzerScpRecvCallback(WOLFSSH* ssh, int state, const char* basePath,
         const char* fileName, int fileMode, word64 mTime, word64 aTime,
         word32 totalFileSz, byte* buf, word32 bufSz, word32 fileOffset,
         void* ctx) {
+    if ( basePath ) /* noret */ memory_test(basePath, strlen(basePath));
+    if ( fileName ) /* noret */ memory_test(fileName, strlen(fileName));
+
+    switch ( state ) {
+        case WOLFSSH_SCP_NEW_REQUEST:
+            return WS_SCP_CONTINUE;
+        case WOLFSSH_SCP_NEW_FILE:
+            return WS_SCP_CONTINUE;
+        case WOLFSSH_SCP_FILE_PART:
+            /* noret */ memory_test(buf, bufSz);
+            return WS_SCP_CONTINUE;
+        case WOLFSSH_SCP_FILE_DONE:
+        case WOLFSSH_SCP_NEW_DIR:
+        case WOLFSSH_SCP_END_DIR:
+            return WS_SCP_CONTINUE;
+    }
+
     return WS_SCP_ABORT;
 }
 
@@ -26,6 +44,74 @@ int fuzzerScpSendCallback(WOLFSSH* ssh, int state, const char* peerRequest,
         char* fileName, word32 fileNameSz, word64* mTime, word64* aTime,
         int* fileMode, word32 fileOffset, word32* totalFileSz, byte* buf,
         word32 bufSz, void* ctx) {
+
+    if ( fileName ) memset(fileName, 0, fileNameSz);
+
+    struct __attribute__((__packed__)) {
+        word64 mTime;
+        word64 aTime;
+        int fileMode;
+        word32 totalFileSz;
+    } fileinfo;
+
+    switch ( state ) {
+        case WOLFSSH_SCP_NEW_REQUEST:
+            return WS_SUCCESS;
+        case WOLFSSH_SCP_SINGLE_FILE_REQUEST:
+            if ( fileNameSz < 2 ) {
+                return WS_SCP_ABORT;
+            }
+
+            fileName[0] = 'x';
+            fileName[1] = 0;
+
+            if ( fuzzer_data_size < sizeof(fileinfo) ) {
+                return WS_SCP_ABORT;
+            }
+
+            memcpy(&fileinfo, fuzzer_data, sizeof(fileinfo));
+            fuzzer_data += sizeof(fileinfo);
+            fuzzer_data_size -= sizeof(fileinfo);
+
+            const int maxReadSize = fileinfo.totalFileSz > bufSz ? bufSz : fileinfo.totalFileSz;
+            const int numRead = fuzzer_recv(NULL, buf, maxReadSize, NULL);
+            if ( numRead < 0 ) {
+                return WS_SCP_ABORT;
+            }
+
+            *mTime = fileinfo.mTime;
+            *aTime = fileinfo.aTime;
+            *fileMode = fileinfo.fileMode;
+            *totalFileSz = fileinfo.totalFileSz;
+
+            return numRead;
+        case WOLFSSH_SCP_RECURSIVE_REQUEST:
+            {
+                uint8_t b;
+                if ( fuzzer_data_size < 1 ) {
+                    return WS_SCP_ABORT;
+                }
+
+                b = *fuzzer_data;
+                fuzzer_data += 1;
+                fuzzer_data_size -= 1;
+
+                switch ( b % 4 ) {
+                    case    0:
+                        return WS_SCP_EXIT_DIR;
+                    case    1:
+                        return WS_SCP_ENTER_DIR;
+                    case    2:
+                        return WS_SCP_EXIT_DIR_FINAL;
+                    case    3:
+                        return WS_SCP_ABORT;
+                }
+            }
+        case WOLFSSH_SCP_CONTINUE_FILE_TRANSFER:
+            memset(buf, 0, bufSz);
+            return bufSz;
+    }
+
     return WS_SCP_ABORT;
 }
 
@@ -59,12 +145,24 @@ FUZZER_RUN_HEADER
 {
     WOLFSSH* ssh = NULL;
     int ret;
+    uint8_t buffer[512];
 
     fuzzer_set_data(data, size);
     ssh = wolfSSH_new(ctx);
 
     if ( (ret = wolfSSH_accept(ssh)) != WS_SUCCESS ) {
         goto end;
+    }
+
+    for (size_t i = 0; i < 5; i++) {
+        ret = wolfSSH_stream_read(ssh, buffer, sizeof(buffer));
+        if ( ret < 0 ) {
+            break;
+        }
+        ret = wolfSSH_stream_send(ssh, buffer, ret);
+        if ( ret < 0 ) {
+            break;
+        }
     }
 
 
