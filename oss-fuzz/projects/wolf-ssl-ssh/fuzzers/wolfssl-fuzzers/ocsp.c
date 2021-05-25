3e5c4135cb5fcfe591d02249b7a5f5d126cdabcc
Guido Vranken <guidovranken@gmail.com>
2021-05-26 00:08:44 +0200

    Fix OCSP fuzzer

--- oss-fuzz/projects/wolf-ssl-ssh/fuzzers/wolfssl-fuzzers/ocsp.c
+++ oss-fuzz/projects/wolf-ssl-ssh/fuzzers/wolfssl-fuzzers/ocsp.c
@@ -9,8 +9,9 @@ FUZZER_INITIALIZE_FOOTER_2
 FUZZER_RUN_HEADER
 {
     OcspResponse resp;
+    OcspEntry single;
     CertStatus status;
-    InitOcspResponse(&resp, &status, data, size);
+    InitOcspResponse(&resp, &single, &status, data, size, NULL);
     OcspResponseDecode(&resp, NULL, NULL, 1);
 }
 FUZZER_RUN_FOOTER
